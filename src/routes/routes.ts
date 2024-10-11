7bfaa2ee5d31d15ed2f7a4df1f56465a43919186
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-10-11 14:56:52 -0300

    adiciona rotas

---
+++ src/routes/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { usuarioController } from '../controllers/usuario';
+import { routes as empresaRoutes } from '../controllers/empresa/routes/routes';
+
+const routes = Router();
+
+routes.put('/login', (req, res, next) => {
+  usuarioController.login(req, res, next);
+});
+
+routes.use(empresaRoutes);
+
+export { routes };
