2c90046da9de4dc120ba91f6dd49f53684a5c52a
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-10-11 14:56:05 -0300

    adiciona model de usuario

---
+++ src/models/Usuario.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Empresa } from './Empresa';
+
+@Entity({ name: 'usuario' })
+export class Usuario {
+
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: number;
+
+  @Column({ name: 'nome' })
+  nome: string;
+
+  @Column({ name: 'email' })
+  email: string;
+
+  @Column({ name: 'senha' })
+  senha: string;
+
+  @Column({ name: 'admin' })
+  admin: boolean;
+
+  @Column({ name: 'data_cadastro', select: false, update: false })
+  dataCadastro: Date;
+
+  @Column({ name: 'ativo' })
+  ativo: boolean;
+
+  @Column({ name: 'data_inativacao', select: false })
+  dataInativacao: Date;
+
+  @ManyToOne(() => Empresa)
+  @JoinColumn({ name: 'empresa_id' })
+  empresa: Empresa;
+}
